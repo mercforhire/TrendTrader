11599c54139f7d0f948ef2607804d725e77d8a06
LeonChen <leonchn84@gmail.com>
2019-12-19 18:26:41 -0500

    Initial Commit

---
+++ AbletrendTrader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AbletrendTrader
+//
+//  Created by Leon Chen on 2019-12-19.
+//  Copyright © 2019 LeonChen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
